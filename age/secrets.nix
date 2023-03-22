97fa85569b9632fd2cb02818d779b320e925c5ed
Paul Stadig <paul@thoughtfull.systems>
2023-03-22 19:28:06 -0400

    WIP

--- age/secrets.nix
+++ age/secrets.nix
@@ -2,8 +2,9 @@ let
   keyfiles = [
     /home/paul/.ssh/id_ed25519.pub
     ./keys/yk5nano475.pub
-    ./keys/mount.pub
-    # ./keys/root.pub
+    ## works
+    # ./keys/mount.pub
+    ./keys/root.pub
   ] ++ (if (builtins.pathExists ./keys/bootstrap.pub)
         then
           [ ./keys/bootstrap.pub ]
