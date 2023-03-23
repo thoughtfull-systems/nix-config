47a2698b222d9684f741f6c04a7a9e067ff5e4fe
Paul Stadig <paul@thoughtfull.systems>
2023-03-23 18:48:40 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -8,7 +8,7 @@ trap 'exec 2>&4 1>&3' 0 1 2 3
 exec 1> >(tee ${logfile}) 2>&1
 
 #### EVERYTHING BELOW WILL GO TO TERMINAL AND LOGFILE ####
-set -exuo pipefail
+set -euo pipefail
 
 git="nix run nixpkgs#git --"
 
