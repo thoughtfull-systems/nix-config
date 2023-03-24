0cf21658e3a895f735053dc4f784e47809170936
Paul Stadig <paul@thoughtfull.systems>
2023-03-24 06:32:10 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -39,7 +39,7 @@ fi
 [[ -v 1 ]] || die "Expected hostname as first argument"
 hostname="${1}"
 
-function indent { sed -E "s/^/    /g;1s/^[[:space:]]*/    /g"; }
+function indent { sed -E 's/\r/\n/g' | sed -E "s/^/    /g"; }
 
 # Checkout hostname branch?
 if (${git} branch -a | grep "${hostname}") &>/dev/null &&
