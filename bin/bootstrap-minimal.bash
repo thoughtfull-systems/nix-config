8581712c76f061f3aa36c0855c6513913577b8a7
Paul Stadig <paul@thoughtfull.systems>
2023-03-29 06:11:42 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -517,7 +517,9 @@ log "Copy hardware config"
 ${ssh} sudo mkdir -p "/mnt/etc/nixos/hosts/${hostname}" |& indent
 ${ssh} sudo mv /mnt/etc/nixos/hardware-configuration.nix \
        "/mnt/etc/nixos/hosts/${hostname}/" |& indent
-${ssh} sudo ${git} add /mnt/etc/nixos/hosts/${hostname}/hardware-configuration.nix |& indent
+${ssh} sudo bash -c cd /mnt/etc/nixos/; \
+  ${git} add /mnt/etc/nixos/hosts/${hostname}/hardware-configuration.nix |&
+  indent
 
 # Install NixOS
 log "Installing NixOS..."
