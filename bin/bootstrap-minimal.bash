d237b9ed6f6e36a9b70b843b6586f94bd1964bff
Paul Stadig <paul@thoughtfull.systems>
2023-04-02 08:43:34 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -496,8 +496,8 @@ fi
 # checkout host branch
 ssh_nixos="${ssh} cd /mnt/etc/nixos;"
 curr=$(${ssh_nixos} sudo git branch --show-current 2>/dev/null)
-echo "${curr}"
-echo "tatenen"
+echo -n "${curr}"
+echo -n "tatenen"
 #curr="tatenen"
 if [[ "${curr}" != "${hostname}" ]] &&
      ${ssh_nixos} sudo git branch -a | grep "${hostname}" &>/dev/null &&
