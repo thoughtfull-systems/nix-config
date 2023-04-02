c9d61c7c21e5699ab2ec112b9fea51a1bc7c2383
Paul Stadig <paul@thoughtfull.systems>
2023-04-02 06:58:43 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -496,7 +496,7 @@ fi
 # checkout host branch
 ssh_nixos="${ssh} cd /mnt/etc/nixos;"
 if (${ssh_nixos} sudo git branch -a | grep "${hostname}") &>/dev/null &&
-     [[ ! $(${ssh_nixos} sudo git branch --show-current 2>/dev/null) = \
+     [[ ! $(${ssh_nixos} sudo git branch --show-current) = \
           "${hostname}" ]] &&
      confirm "Checkout '${hostname}' branch?"
 then
