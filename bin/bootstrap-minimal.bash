fa71e47909d15ed1cb0e45707e079121e9aa2871
Paul Stadig <paul@thoughtfull.systems>
2023-04-03 07:03:43 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -120,7 +120,7 @@ function has_partition {
 }
 
 function is_mounted {
-  ${ssh} mount \| grep \""${1}"\"
+  ${ssh} mount \| grep \""${1}"\" &>/dev/null
 }
 
 function ensure_unmounted {
@@ -134,16 +134,14 @@ function ensure_unmounted {
 }
 
 function ensure_mounted {
-  if ! is_mounted "${2}"; then
-    if ! is_mounted "${1}"; then
-      log "Mounting '${1}'"
-      ${ssh} sudo mount "${1}" "${2}" |& indent ||
-        die "Failed to mount '${1}'"
-    elif ! is_mounted "\$realpath ${1})"; then
-      log "Mounting '${1}'"
-      ${ssh} sudo mount "\$(realpath ${1})" "${2}" |& indent ||
-        die "Failed to mount '${1}'"
-    fi
+  if ! is_mounted "${1}"; then
+    log "Mounting '${1}'"
+    ${ssh} sudo mount "${1}" "${2}" |& indent ||
+      die "Failed to mount '${1}'"
+  elif ! is_mounted "${1}" && ! is_mounted "\$realpath ${1})"; then
+    log "Mounting '${1}'"
+    ${ssh} sudo mount "\$(realpath ${1})" "${2}" |& indent ||
+      die "Failed to mount '${1}'"
   fi
 }
 
