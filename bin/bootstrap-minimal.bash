751fbe8f818ca2f3c4a3681b00a52de0a3b642ca
Paul Stadig <paul@thoughtfull.systems>
2023-04-04 06:14:29 -0400

    WIP

--- bin/bootstrap-minimal.bash
+++ bin/bootstrap-minimal.bash
@@ -408,8 +408,8 @@ if was_partitioned || ! has_lv "root"; then
   ensure_lv_removed "root"
   log "Creating 'root' LVM volume"
   (${ssh} sudo lvcreate --extents 100%FREE --name root ${vg_name} |& indent
-   log "Leaving 256M for LVM snapshots"
-   ${ssh} sudo lvreduce -y --size -256M ${vg_name}/root |& indent
+   log "Leaving free space for LVM snapshots"
+   ${ssh} sudo lvreduce -y --size -300M ${vg_name}/root |& indent
    wait_for "/dev/mapper/${vg_name}-root") ||
     die "Failed to create 'root' LVM volume"
 fi
