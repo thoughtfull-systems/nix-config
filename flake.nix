d2814ac6b2c2aec072a73d61ffcfdc3bbd80541c
Paul Stadig <paul@thoughtfull.systems>
2023-04-04 06:49:02 -0400

    WIP

--- flake.nix
+++ flake.nix
@@ -1,11 +1,25 @@
 {
   description = "NixOS configuration";
   inputs = {
+    agenix.url = "github:thoughtfull-systems/agenix";
     hardware.url = "github:nixos/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     # for some software I want the most recent version
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
   };
   outputs = inputs: {
+    nixosConfigurations = {
+      tatenen = inputs.nixpkgs.lib.nixosSystem {
+        modules = [ ./hosts/tatenen ];
+        specialArgs = {
+          agenix = inputs.agenix;
+          unstable = import inputs.unstable {
+            config.allowUnfree = true;
+            system = "x86_64-linux";
+          };
+        };
+        system = "x86_64-linux";
+      };
+    };
   };
 }
