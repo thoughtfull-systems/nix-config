de1a650daa60d99cb6fcd7e6550b1a1d37b6ed27
Paul Stadig <paul@thoughtfull.systems>
2023-04-15 17:36:23 -0400

    Pin agenix (there aren't any recent tags)

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   description = "NixOS configuration";
   inputs = {
-    agenix.url = "github:ryantm/agenix";
+    agenix.url = "github:ryantm/agenix/e64961977f60388dd0b49572bb0fc453b871f896";
     hardware.url = "github:nixos/nixos-hardware/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     # for some software I want the most recent version
