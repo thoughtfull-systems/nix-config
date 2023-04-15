5dc137f0cf3e2691d6785af95b2289144baded61
Paul Stadig <paul@thoughtfull.systems>
2023-04-15 17:46:46 -0400

    Add home manager dependency

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
   inputs = {
     agenix.url = "github:ryantm/agenix/e64961977f60388dd0b49572bb0fc453b871f896";
     hardware.url = "github:nixos/nixos-hardware/master";
+    home-manager = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/home-manager/release-22.11";
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     # for some software I want the most recent version
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -12,7 +16,7 @@
       ziph = inputs.nixpkgs.lib.nixosSystem {
         modules = [ ./hosts/ziph ];
         specialArgs = {
-          agenix = inputs.agenix;
+          inherit (inputs) agenix home-manager;
           unstable = import inputs.unstable {
             config.allowUnfree = true;
             system = "x86_64-linux";
