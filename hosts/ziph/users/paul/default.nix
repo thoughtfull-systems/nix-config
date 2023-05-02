32a1cdbe02f9a1ffa67f04a90e66070170df3a62
Paul Stadig <paul@thoughtfull.systems>
2023-05-02 11:13:54 -0400

    Merge home-manager config into user nixos module

--- hosts/ziph/users/paul/default.nix
+++ hosts/ziph/users/paul/default.nix
@@ -1,5 +1,33 @@
 { config, pkgs, ... } : {
-  imports = [ ./home-manager.nix ];
+  home-manager.users.paul = {
+    home = {
+      homeDirectory = "/home/paul";
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "22.11";
+      username = "paul";
+    };
+    imports = [
+      ../zsh.nix
+      ./exwm.nix
+    ];
+    programs = {
+      emacs = {
+        enable = true;
+        extraPackages = epkgs: with epkgs; [
+          exwm
+        ];
+      };
+      # Let Home Manager install and manage itself.
+      home-manager.enable = true;
+    };
+  };
   users.users.paul = {
     description = "Paul Stadig";
     extraGroups = [ "networkmanager" "wheel" ];
--- hosts/ziph/users/root/default.nix
+++ hosts/ziph/users/root/default.nix
@@ -1,5 +1,25 @@
 { pkgs, ... } : {
-  imports = [ ./home-manager.nix ];
+  home-manager.users.root = {
+    home = {
+      homeDirectory = "/root";
+      # This value determines the Home Manager release that your
+      # configuration is compatible with. This helps avoid breakage
+      # when a new Home Manager release introduces backwards
+      # incompatible changes.
+      #
+      # You can update Home Manager without changing this value. See
+      # the Home Manager release notes for a list of state version
+      # changes in each release.
+      stateVersion = "22.11";
+      username = "root";
+    };
+    imports = [ ../zsh.nix ];
+    programs = {
+      emacs.enable = true;
+      # Let Home Manager install and manage itself.
+      home-manager.enable = true;
+    };
+  };
   users.users.root = {
     openssh.authorizedKeys.keys = [
       "ecdsa-sha2-nistp521 AAAAE2VjZHNhLXNoYTItbmlzdHA1MjEAAAAIbmlzdHA1MjEAAACFBAB2hfargabOYq6TZ+U6zXUZG+SWrxWdV0Fq5AbhDLghAL4kdwi1j5Q9C8ki622ZwIkk+v7+575IXgyezlHIHjIFFwDf09ODfTPVSwNizpRBK8uMX1YV0XpULJmV8nOJFF0gbn9gQNktM6Obfuhl7QBGhmpEvnvROsBaAqU8OqcQeMRg+w== paul@carbon"
