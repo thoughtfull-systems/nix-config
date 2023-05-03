5156016b308d631b735b9d04215dba1b49ad63af
Paul Stadig <paul@thoughtfull.systems>
2023-05-03 17:35:03 -0400

    Magit is also part of general programming configuration

--- hosts/ziph/users/emacs/my-prog.nix
+++ hosts/ziph/users/emacs/my-prog.nix
@@ -2,11 +2,12 @@
   imports = [ ../emacs.nix ];
   programs.emacs = {
     extraConfig = ''
+      (use-package magit :bind ("C-x g" . magit-status))
       (use-package my-prog :after prog-mode)
     '';
     extraPackages = epkgs: with epkgs; [
+      magit
       my-prog
-      paredit
     ];
   };
 }
--- my-elisp/my-prog/default.nix
+++ my-elisp/my-prog/default.nix
@@ -1,5 +1,8 @@
 self: super: {
   my-prog = self.elpaBuild {
+    packageRequires = with self; [
+      paredit
+    ];
     pname = "my-prog";
     src = ./my-prog.el;
     version = "0.0.0";
