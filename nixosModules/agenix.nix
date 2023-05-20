2d6353d475dcd03158a552756133f5807933f026
Paul Stadig <paul@ziph.stadig.name>
2023-05-20 06:44:08 -0400

    Use bootstrap key if it is available

--- nixosModules/agenix.nix
+++ nixosModules/agenix.nix
@@ -1,5 +1,8 @@
 { agenix, pkgs, ... } : {
-  age.identityPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
+  age.identityPaths = [
+    "/etc/ssh/ssh_host_ed25519_key"
+    "/tmp/bootstrap.key"
+  ];
   imports = [ agenix.nixosModule ];
   environment.systemPackages = [
     agenix.package
