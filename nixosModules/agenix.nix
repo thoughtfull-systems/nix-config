95bb86b8f29ac2c8b642d1d7983cc81a5de5aaf2
Paul Stadig <paul@thoughtfull.systems>
2023-05-26 07:27:42 -0400

    WIP

--- nixosModules/agenix.nix
+++ nixosModules/agenix.nix
@@ -3,6 +3,11 @@
     "/etc/ssh/ssh_host_ed25519_key"
     "/etc/nixos/age/yk5nano475.txt"
   ];
+  nixpkgs.overlays = [
+    (self: super: {
+      rage = super.rage // super.age-plugin-yubikey;
+    })
+  ];
   imports = [ agenix.nixosModule ];
   environment.systemPackages = [
     agenix.package
