3e60975cff8dccb14588cdc2190c8ade9c72daea
Paul Stadig <paul@thoughtfull.systems>
2023-06-02 12:23:33 -0400

    WIP

--- bin/bootstrap.bash
+++ bin/bootstrap.bash
@@ -7,38 +7,56 @@ exec 1> >(tee ${logfile}) 2>&1
 ### EVERYTHING BELOW WILL GO TO CONSOLE AND LOGFILE ############################
 set -euo pipefail
 
-function log { echo "=== ${1}"; }
-
-log "Installation starting $(date)"
-
-## VERIFY ##
-function die { echo "!!! ${1}" >&2; exit 1; }
-
-[[ -v 1 ]] || die "Expected hostname as first argument"
-hostname="${1}"
-
-boot_part="${hostname}-boot"
-boot_device="/dev/disk/by-partlabel/${boot_part}"
-[[ -b "${boot_device}" ]] ||
-  die "Missing partition ${boot_part}"
-
-luks_part="${hostname}-luks"
-luks_device="/dev/disk/by-partlabel/${luks_part}"
-[[ -b "${luks_device}" ]] ||
-  die "Missing parttion ${luks_part}"
-
-lvm_name="${hostname}-lvm"
-lvm_device="/dev/mapper/${lvm_name}"
-
+function log { printf "%s === %s\n" "$(date -uIns)" "${1}"; }
+function die { printf "%s !!! %s\n" "$(date -uIns)" "${1}" >&2; exit 1; }
+function ts {
+  while read -r line; do
+    printf '%s %s\n' "$(date -uIns)" "${line}";
+  done
+}
+function try_ts {
+  # a bit complicated, but captures both stdout and stderr in (pretty much) printed order, and
+  # displays only stdout on success and both stdout and stderr on failure.
+  out=$(mktemp)
+  err=$(mktemp)
+  dir=$(mktemp -d)
+  # the fifos are necessary so we can have some subprocess for which to wait, otherwise the outputs
+  # aren't ready when we try to cat them
+  outp="${dir}/out"
+  mkfifo ${outp}
+  (cat ${outp} >${out}) &
+  outpid=$!
+  errp="${dir}/err"
+  mkfifo ${errp}
+  (cat ${errp} >${err}) &
+  errpid=$!
+  if (eval "${1}") 1> >(ts > ${outp}) 2> >(ts > ${errp}); then
+    wait ${outpid} ${errpid}; rm -rf "${dir}"
+    cat ${out} | sort
+    rm ${out} ${err}
+  else
+    result=$?
+    wait ${outpid} ${errpid}; rm -rf "${dir}"
+    cat ${out} ${err} | sort
+    rm ${out} ${err}
+    return $result
+  fi
+}
+function try {
+  # cut off the timestamp at the beginning of each line
+  run_ts "${1}" > >(cut -c 37-)
+}
+function indent {
+  while read -r line; do
+    printf '    %s\n' "${line}";
+  done
+}
 function ask_no_echo() {
   msg="??? ${1} "
   read -sp "${msg}" ${2}
   # prevents bunching in the log (because input is not logged)
   echo
 }
-
-function indent { sed -E 's/\r$//g;s/\r/\n/g' | sed -E "s/^/    /g"; }
-
 function wait_for() {
   if [[ ! -e "${1}" ]] &>/dev/null; then
     log "Waiting for '${1}'..."
@@ -48,6 +66,28 @@ function wait_for() {
   fi
 }
 
+
+log "Installation started"
+
+## VERIFY ##
+
+[[ -v 1 ]] || die "Expected hostname as first argument"
+hostname="${1}"
+
+boot_part="${hostname}-boot"
+boot_device="/dev/disk/by-partlabel/${boot_part}"
+[[ -b "${boot_device}" ]] ||
+  die "Missing partition: ${boot_part}"
+
+luks_part="${hostname}-luks"
+luks_device="/dev/disk/by-partlabel/${luks_part}"
+[[ -b "${luks_device}" ]] ||
+  die "Missing partition: ${luks_part}"
+
+lvm_name="${hostname}-lvm"
+lvm_device="/dev/mapper/${lvm_name}"
+
+log "Opening LVM device: ${lvm_device}"
 if [[ ! -b "${lvm_device}" ]]; then
   ask_no_echo "Enter passphrase for ${luks_device}:" PASS
   while ! echo "${PASS}" |
@@ -58,14 +98,15 @@ if [[ ! -b "${lvm_device}" ]]; then
   wait_for "/dev/mapper/${lvm_name}"
 fi
 
-(pvs | grep "${lvm_device}") &>/dev/null || die "Missing physical volume ${lvm_device}"
+(pvs | grep "${lvm_device}") &>/dev/null || die "Missing physical volume: ${lvm_device}"
 
 vg_name="${hostname}"
-(vgs | grep "${vg_name}") &>/dev/null || die "Missing volume group ${vg_name}"
+(vgs | grep "${vg_name}") &>/dev/null || die "Missing volume group: ${vg_name}"
 
 function verify_lv {
-  (lvs -S "vg_name=${vg_name} && lv_name=${1}" |
-     grep "${1}") &>/dev/null || die "Missing logical volume ${1}"
+  try <<-EOF || die "Missing logical volume: ${1}"
+    lvs -S "vg_name=${vg_name} && lv_name=${1}" | grep "${1}"
+  EOF
 }
 
 verify_lv "root"
