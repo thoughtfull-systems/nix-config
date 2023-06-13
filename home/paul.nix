8462f05d0570a885c9328c6dca2a056041f42695
Paul Stadig <paul@thoughtfull.systems>
2023-06-13 06:53:45 -0400

    Revert "Enable home-manager ssh configuration"
    This reverts commit 3f8d675c2e8a087de4f077f93167c52b08cdfa55.

--- home/paul.nix
+++ home/paul.nix
@@ -18,10 +18,6 @@
       enable = true;
       ignores = [ "*~" ];
     };
-    ssh = {
-      enable = true;
-      forwardAgent = true;
-      matchBlocks."*".identityFile = "/home/paul/.ssh/id_ed25519_sk_rk_yk5nano475";
-    };
+    ssh.matchBlocks."*".identityFile = [ "~/.ssh/id_ed25519_sk_rk_yk5nano475" ];
   };
 }
