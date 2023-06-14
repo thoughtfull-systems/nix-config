a6e59e8ab341560297cc815a3c48f482afc19b06
Paul Stadig <paul@thoughtfull.systems>
2023-06-14 07:05:32 -0400

    Configure proton vpn

---
+++ homeManagerModules/proton-vpn.nix
@@ -0,0 +1,11 @@
+{ config, lib, ... }: {
+  services = lib.mkDefault {
+    openvpn.servers.proton = {
+      config = ''
+        config #{config.age.secrets.proton.ovpn.path}
+        auth-user-pass #{config.age.secrets.proton.txt.path}
+      '';
+      updateResolvConf = true;
+    };
+  };
+}
