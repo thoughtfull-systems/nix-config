9a2a58e313610b79d73feb130bac1184924af756
Paul Stadig <paul@thoughtfull.systems>
2023-06-14 05:40:15 -0400

    More root config

--- nixos/root.nix
+++ nixos/root.nix
@@ -1,6 +1,8 @@
-{ pkgs, ... } : {
-  users.users.root = {
+{ lib, pkgs, ... } : {
+  services.openssh.permitRootLogin = lib.mkDefault "prohibit-password";
+  users.users.root = lib.mkDefault {
     openssh.authorizedKeys.keys = import ./paul/authorizedKeys;
+    password = null;
     shell = pkgs.zsh;
   };
 }
