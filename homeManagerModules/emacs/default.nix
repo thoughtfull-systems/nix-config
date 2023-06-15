870c23e99af4208fa26542e38d70b31540453553
Paul Stadig <paul@thoughtfull.systems>
2023-06-15 16:45:46 -0400

    Move emacs init to actual init.el file

--- homeManagerModules/emacs/default.nix
+++ homeManagerModules/emacs/default.nix
@@ -3,25 +3,27 @@
 in {
   options.thoughtfull.emacs.enable = lib.mkEnableOption "emacs";
   config = lib.mkIf cfg.enable {
-    home.packages = with pkgs; [
-      aspell
-      aspellDicts.en
-      aspellDicts.en-computers
-      aspellDicts.en-science
-      emacs-all-the-icons-fonts
-      silver-searcher
-      source-code-pro
-      # for loading files from JARs
-      unzip
-    ];
-    programs.emacs = {
-      enable = true;
-      extraConfig = lib.mkBefore (builtins.readFile ./init.el);
-      extraPackages = epkgs: with epkgs; [
-        my
-        my-completion
-        my-prog
+    home = {
+      file.".config/emacs/init.el".source = ./init.el;
+      packages = with pkgs; [
+        aspell
+        aspellDicts.en
+        aspellDicts.en-computers
+        aspellDicts.en-science
+        emacs-all-the-icons-fonts
+        silver-searcher
+        source-code-pro
+        # for loading files from JARs
+        unzip
       ];
+      programs.emacs = {
+        enable = true;
+        extraPackages = epkgs: with epkgs; [
+          my
+          my-completion
+          my-prog
+        ];
+      };
     };
   };
 }
