6e7f1dabd0ab5102171a480fb09b3ce50877747f
Paul Stadig <paul@thoughtfull.systems>
2023-06-16 05:26:27 -0400

    Include better defaults for nix auto upgrade

--- nixosModules/nix.nix
+++ nixosModules/nix.nix
@@ -21,7 +21,7 @@ in {
         type = lib.types.str;
       };
       inputs = lib.mkOption {
-        default = [ "nixpkgs" ];
+        default = [ "nixpkgs" "nixpkgs-unstable" "nixos-hardware" ];
         description = lib.mdDoc "Flake inputs to update for upgrades.";
         type = lib.types.listOf lib.types.str;
       };
