cd56b2170305ea885f25693a99e61609949b62fc
Paul Stadig <paul@thoughtfull.systems>
2023-06-18 09:21:07 -0400

    Identity paths default is sufficient

--- lib/thoughtfull-system/agenix.nix
+++ lib/thoughtfull-system/agenix.nix
@@ -1,7 +1,4 @@
 agenix: { pkgs, ... } : {
-  age.identityPaths = [
-    "/etc/ssh/ssh_host_ed25519_key"
-  ];
   environment.systemPackages = [
     agenix.packages.${pkgs.system}.default
     pkgs.age-plugin-yubikey
