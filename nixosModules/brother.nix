d043092b91fcc60a9ed8756bdfdc5aa34ca304fe
Paul Stadig <paul@thoughtfull.systems>
2023-06-19 06:45:34 -0400

    Simplify brother

--- nixosModules/brother.nix
+++ nixosModules/brother.nix
@@ -1,19 +1,13 @@
-{ config, lib, pkgs, ... }: let
-  cfg = config.thoughtfull.brother;
-in {
-  options.thoughtfull.brother.enable = lib.mkEnableOption "brother";
-  config = lib.mkIf cfg.enable {
-    hardware.printers.ensurePrinters = [
-      {
-        name = "brother-mfc-l2750dw";
-        deviceUri = "ipp://brother.lan:631/";
-        model = "everywhere";
-        ppdOptions.PageSize = "Letter";
-      }
-    ];
-    services.printing = {
-      enable = true;
-      drivers = [ pkgs.cups-brother-mfcl2750dw ];
-    };
+{ config, lib, pkgs, ... }: lib.mkIf config.services.printing.enable {
+  hardware.printers.ensurePrinters = [
+    {
+      name = "brother-mfc-l2750dw";
+      deviceUri = "ipp://brother.lan:631/";
+      model = "everywhere";
+      ppdOptions.PageSize = "Letter";
+    }
+  ];
+  services.printing = {
+    drivers = [ pkgs.cups-brother-mfcl2750dw ];
   };
 }
