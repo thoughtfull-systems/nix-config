d0d3a7e3d7624296380c863acd1024797df834ab
Paul Stadig <paul@thoughtfull.systems>
2023-06-19 08:24:33 -0400

    Fix home-manager emacs module

--- homeManagerModules/emacs/default.nix
+++ homeManagerModules/emacs/default.nix
@@ -1,21 +1,23 @@
 { config, lib, pkgs, ... }: lib.mkIf config.programs.emacs.enable {
   home = {
     file.".config/emacs/init.el".source = ./init.el;
-    packages = (with pkgs; [
-      aspell
-      aspellDicts.en
-      aspellDicts.en-computers
-      aspellDicts.en-science
-      silver-searcher
-      # for loading files from JARs
-      unzip
-    ]) ++ (lib.mkIf config.thoughtfull.desktop.enable (with pkgs; [
-      emacs-all-the-icons-fonts
-      source-code-pro
-    ]));
+    packages = (lib.mkMerge [
+      (with pkgs; [
+        aspell
+        aspellDicts.en
+        aspellDicts.en-computers
+        aspellDicts.en-science
+        silver-searcher
+        # for loading files from JARs
+        unzip
+      ])
+      (lib.mkIf config.thoughtfull.desktop.enable (with pkgs; [
+        emacs-all-the-icons-fonts
+        source-code-pro
+      ]))
+    ]);
   };
   programs.emacs = {
-    enable = true;
     extraPackages = epkgs: with epkgs; [
       my
       my-completion
