97ca63478052677b4d2a82e9004a93574fc34114
Paul Stadig <paul@thoughtfull.systems>
2023-06-20 21:43:45 -0400

    Fix brightness scripts

--- packages/brightness/default.nix
+++ packages/brightness/default.nix
@@ -1,11 +1,36 @@
-{ nixpkgs, ... }: nixpkgs.stdenv.mkDerivation {
-  builder = nixpkgs.writeScript "builder.sh" ''
-    source $stdenv/setup
-    mkdir -p $out/bin
-    cp $src/brightness-* $out/bin
-    chmod +x $out/bin/*
-  '';
-  pname = "brightness";
-  src = ./.;
-  version = "1.0.0";
+{ nixpkgs, ... }: let
+  xbacklight = "${nixpkgs.xorg.xbacklight}/bin/xbacklight";
+  status = nixpkgs.substituteAll {
+    dir = "bin";
+    inherit xbacklight;
+    isExecutable = true;
+    src = ./brightness-status;
+  };
+  notify = nixpkgs.substituteAll {
+    dir = "bin";
+    isExecutable = true;
+    src = ./brightness-status-notify;
+    notify = "${nixpkgs.notify-desktop}/bin/notify-desktop";
+    status = "${status}/bin/brightness-status";
+  };
+in nixpkgs.symlinkJoin {
+  name = "brightness";
+  paths = [
+    status
+    notify
+    (nixpkgs.substituteAll {
+      dir = "bin";
+      isExecutable = true;
+      src = ./brightness-up;
+      inherit xbacklight;
+      notify = "${notify}/bin/brightness-status-notify";
+    })
+    (nixpkgs.substituteAll {
+      dir = "bin";
+      isExecutable = true;
+      src = ./brightness-down;
+      inherit xbacklight;
+      notify = "${notify}/bin/brightness-status-notify";
+    })
+  ];
 }
