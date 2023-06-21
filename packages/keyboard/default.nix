ec5de9f95b2fccad71310c4dca4c6cde5aaad89d
Paul Stadig <paul@thoughtfull.systems>
2023-06-20 21:44:24 -0400

    Update keyboard script installation

--- packages/keyboard/default.nix
+++ packages/keyboard/default.nix
@@ -1,11 +1,19 @@
-{ nixpkgs, ... }: nixpkgs.stdenv.mkDerivation {
-  builder = nixpkgs.writeScript "builder.sh" ''
-    source $stdenv/setup
-    mkdir -p $out/bin
-    cp $src/keyboard-* $out/bin
-    chmod +x $out/bin/*
-  '';
-  pname = "keyboard";
-  src = ./.;
-  version = "1.0.0";
+{ nixpkgs, ... }: let
+  setxkbmap = "${nixpkgs.xorg.setxkbmap}/bin/setxkbmap";
+in nixpkgs.symlinkJoin {
+  name = "keyboard";
+  paths = [
+    (nixpkgs.substituteAll {
+      dir = "bin";
+      isExecutable = true;
+      src = ./keyboard-status;
+      inherit setxkbmap;
+    })
+    (nixpkgs.substituteAll {
+      dir = "bin";
+      isExecutable = true;
+      src = ./keyboard-toggle;
+      inherit setxkbmap;
+    })
+  ];
 }
