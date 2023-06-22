f14bbd428f704f7cf4f3ca0c80c4234b94e44b6f
Paul Stadig <paul@thoughtfull.systems>
2023-06-22 09:00:27 -0400

    Formatting

--- home/ziph/paul.nix
+++ home/ziph/paul.nix
@@ -3,20 +3,18 @@
     thoughtfull.paul
     thoughtfull.syncthing
   ];
-  thoughtfull = {
-    services.syncthing-init.folders = {
-      org = {
-        devices = [ "hemera" ];
-        enable = true;
-      };
-      org-work = {
-        devices = [ "hemera" ];
-        enable = true;
-      };
-      sync = {
-        devices = [ "hemera" ];
-        enable = true;
-      };
+  thoughtfull.services.syncthing-init.folders = {
+    org = {
+      devices = [ "hemera" ];
+      enable = true;
+    };
+    org-work = {
+      devices = [ "hemera" ];
+      enable = true;
+    };
+    sync = {
+      devices = [ "hemera" ];
+      enable = true;
     };
   };
 }
