e02cc3113ae2244fc54ea4636da5b3478d3e3b6c
Paul Stadig <paul@thoughtfull.systems>
2023-06-28 15:23:29 -0400

    Include configver in xfce4-panel config

--- homeManagerModules/thoughtfull/desktop/xfce.nix
+++ homeManagerModules/thoughtfull/desktop/xfce.nix
@@ -4,15 +4,16 @@
 in lib.mkIf enable {
   xfconf.settings = {
     xfce4-panel = {
+      "configver" = 2;
       "panels" = [ 1 ];
       "panels/dark-mode" = true;
-      "panels/panel-1/position" = "p=6;x=0;y=0";
-      "panels/panel-1/length" = 100;
-      "panels/panel-1/position-locked" = true;
       "panels/panel-1/icon-size" = 16;
-      "panels/panel-1/size" = 26;
-      "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 9 10 11 12 13 14 ];
+      "panels/panel-1/length" = 100;
       "panels/panel-1/mode" = 0;
+      "panels/panel-1/plugin-ids" = [ 1 2 3 4 5 6 9 10 11 12 13 14 ];
+      "panels/panel-1/position" = "p=6;x=0;y=0";
+      "panels/panel-1/position-locked" = true;
+      "panels/panel-1/size" = 26;
       "plugins/plugin-1" = "applicationsmenu";
       "plugins/plugin-2" = "tasklist";
       "plugins/plugin-2/grouping" = 1;
@@ -23,12 +24,12 @@ in lib.mkIf enable {
       "plugins/plugin-5" = "separator";
       "plugins/plugin-5/style" = 0;
       "plugins/plugin-6" = "systray";
-      "plugins/plugin-6/square-icons" = true;
       "plugins/plugin-6/known-legacy-items" = [
         "networkmanager applet"
         "xfce4-power-manager"
         "ethernet network connection \"wired connection 1\" active"
       ];
+      "plugins/plugin-6/square-icons" = true;
       "plugins/plugin-8" = "pulseaudio";
       "plugins/plugin-8/enable-keyboard-shortcuts" = true;
       "plugins/plugin-8/show-notifications" = true;
