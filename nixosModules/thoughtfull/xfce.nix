124e43b8287fe22e45fcc0931bd363e7032ff657
Paul Stadig <paul@thoughtfull.systems>
2023-06-28 15:28:50 -0400

    Get a prettier cursor on xfce4-panel

--- nixosModules/thoughtfull/xfce.nix
+++ nixosModules/thoughtfull/xfce.nix
@@ -2,7 +2,8 @@
 lib.mkIf config.services.xserver.desktopManager.xfce.enable {
   environment.systemPackages = [ pkgs.xfce.xfce4-panel ];
   services.xserver.desktopManager.xfce = {
-    noDesktop = lib.mkDefault true;
+    # without desktop I get the default X cursor over the panel; not a big deal, but I don't like it
+    noDesktop = lib.mkDefault false;
     enableXfwm = lib.mkDefault false;
   };
 }
