b79159d21569ddcf4aeadbd626f353969f3bd87e
Paul Stadig <paul@thoughtfull.systems>
2023-06-28 16:14:08 -0400

    Gracefully choose appropriate yubioath gui package

--- nixosModules/thoughtfull/yubikey.nix
+++ nixosModules/thoughtfull/yubikey.nix
@@ -3,8 +3,14 @@
     age-plugin-yubikey
     yubikey-manager
     yubikey-manager-qt
-    yubioath-flutter
-  ];
+  ] ++
+  (if pkgs ? yubioath-flutter
+   then
+     # 23.05+
+     [ yubioath-flutter ]
+   else
+     # 22.11
+     [ yubioath-desktop ]);
   hardware.gpgSmartcards.enable = true;
   services.pcscd.enable = true;
 }
