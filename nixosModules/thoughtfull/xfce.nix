876de4fc431fb35a2478380b9efbd86533894f15
Paul Stadig <paul@thoughtfull.systems>
2023-06-29 17:30:18 -0400

    Since I'm not disabling XFCE desktop I don't need to install panel

--- nixosModules/thoughtfull/xfce.nix
+++ nixosModules/thoughtfull/xfce.nix
@@ -1,6 +1,5 @@
 { config, lib, pkgs, ... }:
 lib.mkIf config.services.xserver.desktopManager.xfce.enable {
-  environment.systemPackages = [ pkgs.xfce.xfce4-panel ];
   services.xserver.desktopManager.xfce = {
     # without desktop I get the default X cursor over the panel; not a big deal, but I don't like it
     noDesktop = lib.mkDefault false;
