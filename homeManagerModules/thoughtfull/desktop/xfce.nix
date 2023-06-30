970b97e483491ad8b8beb23dd9d07202df1163f1
Paul Stadig <paul@thoughtfull.systems>
2023-06-30 16:01:30 -0400

    Disable trackpad point to click

--- homeManagerModules/thoughtfull/desktop/xfce.nix
+++ homeManagerModules/thoughtfull/desktop/xfce.nix
@@ -18,6 +18,9 @@ in {
   config = lib.mkIf enable {
     xfconf = {
       settings = {
+        pointers = {
+          "ELAN067600_04F33195_Touchpad/Properties/libinput_Tapping_Enabled" = 0;
+        };
         xfce4-panel = {
           "configver" = 2;
           "panels" = [ 1 ];
